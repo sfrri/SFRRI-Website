81ade7d9f16e302925ac77b56d4771a8403ae078
Daniel Fraser <dan@danielfraser.me>
2024-03-22 14:57:31 +0000

    footer component

---
+++ src/app/components/Footer.js
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { Paper, Box } from '@mui/material/';
+
+const Footer = () => {
+    return (
+        <Box
+            component="footer"
+            sx={{
+                py: 3,
+                px: 2,
+                mt: 'auto',
+                //backgroundColor: backgroundColor(theme)
+            }}
+        >
+            
+        </Box>
+    )
+}
