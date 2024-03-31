c4189a7cc1d773ac40dbb2aa543eccf78266e5e9
Daniel Fraser <dan@danielfraser.me>
2024-03-31 16:11:20 +0100

    updated news

--- src/app/components/News.js
+++ src/app/components/News.js
@@ -33,6 +33,12 @@ export const News1 = () => {
           <span className="italic">Location:</span> Savannah Convention Center, Savannah, Georgia, USA<br />
           <span className="italic">Further information:</span> <a href="https://sfrbm.org/" target="_blank">sfrbm.org</a>
         </p>
+        <p>
+          <span className="italic">May 29-31, 2024</span><br />
+          <span className="bold">16<sup>th</sup> International Conference on Reactive Oxygen and Nitrogen Species in Plants</span><br />
+          <span className="italic">Location:</span> Antibes Juan-les-Pins, France<br />
+          <span className="italic">Further information:</span> <a href="https://www.alphavisa.com/pog/2024/index.php" target="_blank">https://www.alphavisa.com/pog/2024/</a>
+        </p>
         <hr />
       </>
       )
@@ -42,10 +48,17 @@ export const News2 = () => {
     return (
         <>
         <p>
-          <span className="italic">May 29-31, 2024</span><br />
-          <span className="bold">16<sup>th</sup> International Conference on Reactive Oxygen and Nitrogen Species in Plants</span><br />
-          <span className="italic">Location:</span> Antibes Juan-les-Pins, France<br />
-          <span className="italic">Further information:</span> <a href="https://www.alphavisa.com/pog/2024/index.php" target="_blank">https://www.alphavisa.com/pog/2024/</a>
+          <span className="italic">October 10-12, 2024</span><br />
+          <span className="bold">Lipid Oxidation: New Biochemical Insights and Clinical Applications</span><br />
+          <span className="italic">Location:</span> Genoa, Italy<br />
+          <span className="italic">Further information:</span> <a href="downloads/HNE-Club-Meeting.pdf" target="_blank">download flyer</a>
+        </p>
+        <hr />
+        <p>
+          <span className="italic">October 21-23, 2024</span><br />
+          <span className="bold">11<sup>th</sup> Biennial Meeting of SFRR Asia and Chinese National Conference of Redox Biology & Medicine 2024</span><br />
+          <span className="italic">Location:</span> <a href="http://www.kuntaihotel.com/" target="_blank">Kuntai Hotel</a> (Beijing Wangjing), Beijing, China <br />
+          <span className="italic">Further information:</span> <a href="http://www.sfrr-asia2024.com.cn/" target="_blank">http://www.sfrr-asia2024.com.cn/</a>
         </p>
         <hr />
         <p>
