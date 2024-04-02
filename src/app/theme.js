3d86da0d93d33cbd1ac064ce30e6e44449a8a663
Daniel Fraser <dan@danielfraser.me>
2024-04-02 14:18:22 +0100

    added font

--- src/app/theme.js
+++ src/app/theme.js
@@ -1,8 +1,6 @@
 'use client';
 import { Roboto } from 'next/font/google';
 import { createTheme } from '@mui/material/styles';
-//import AvenirNextLTProRegularWoff from '/static/fonts/AvenirNextLTPro-Regular.woff';
-//import AvenirNextLTProBoldWoff from '/static/fonts/AvenirNextLTPro-Bold.woff';
 
 const roboto = Roboto({
   weight: ['300', '400', '500', '700'],
@@ -22,26 +20,6 @@ const theme = createTheme({
   },
   components: {
     // Name of the component
-    /*MuiCssBaseline: {
-      styleOverrides: `
-        @font-face {
-          font-family: 'AvenirNextLTPro';
-          font-style: normal;
-          font-display: swap;
-          font-weight: 400;
-          src: local('AvenirNextLTPro'), local('AvenirNextLTProRegular'), url(${AvenirNextLTProRegularWoff}) format('woff');
-          unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
-        },
-        @font-face {
-            font-family: 'AvenirNextLTPro';
-            font-style: normal;
-            font-display: swap;
-            font-weight: 400;
-            src: local('AvenirNextLTPro'), local('AvenirNextLTProBold'), url(${AvenirNextLTProBoldWoff}) format('woff');
-            unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
-          }
-      `,
-    },*/
     MuiPaper: {
       /*styleOverrides: {
         // Name of the slot
