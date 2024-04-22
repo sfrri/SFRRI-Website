09b7a8c1084d2b21209552ee9f8f5355073a7292
Daniel Fraser <dan@danielfraser.me>
2024-04-22 19:28:02 +0100

    using decap cms data

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,6 +1,10 @@
 import * as React from 'react';
 import Paper from '@mui/material/Paper';
 import Link from '@mui/material/Link';
+import * as data from '../../../_data/contact.json'
+import Markdown from 'react-markdown'
+import rehypeExternalLinks from 'rehype-external-links'
+import rehypeRaw from 'rehype-raw'
 
 export default async function Contact() {
     "use server";
@@ -8,7 +12,8 @@ export default async function Contact() {
         <main>
             <Paper variant="padded" elevation={3}>
                 <h2>Contact</h2>
-                <h4>Professor Giovanni E. Mann</h4>
+                <Markdown rehypePlugins={[[rehypeExternalLinks, { target: '_blank' }], [rehypeRaw]]} className='pNoMarginBottom'>{data.body}</Markdown>
+                {/* <h4>Professor Giovanni E. Mann</h4>
                 <p>Secretary-General SFRRI<br />
                     c/o Cardiovascular Division<br />
                     BHF Centre of Research Excellence<br />
@@ -20,7 +25,7 @@ export default async function Contact() {
                     UK </p>
                 <p>Tel: +44-20-7848-4306<br />
                     Fax: +44-20-7848-4500<br />
-                    <Link href="mailto:giovanni.mann@kcl.ac.uk">giovanni.mann@kcl.ac.uk</Link></p>
+                    <Link href="mailto:giovanni.mann@kcl.ac.uk">giovanni.mann@kcl.ac.uk</Link></p> */}
                 <p>Website designed and developed by <Link href='https://danielfraser.me' target="_blank">Daniel Fraser</Link></p>
             </Paper>
         </main>
