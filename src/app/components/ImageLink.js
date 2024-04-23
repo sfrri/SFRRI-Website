5cc9aa14bb1a07749876be5c32d13a47580391f0
Daniel Fraser <dan@danielfraser.me>
2024-04-23 22:25:02 +0100

    experimenting with load times

--- src/app/components/ImageLink.js
+++ src/app/components/ImageLink.js
@@ -5,11 +5,13 @@ import CardMedia from '@mui/material/CardMedia';
 import Box from '@mui/material/Box';
 import Link from '@mui/material/Link';
 import { kebabize } from '../../utils';
+import Paper from '@mui/material/Paper';
+import Image from 'next/image';
 
 export const ImageLink = ({ linkName, bgImgUrl }) => {
     return (
         <Link href={`/${kebabize(linkName)}`}>
-            <Card
+            {/* <Card
                 sx={{ 
                     borderRadius: '3px', 
                     position: 'relative',
@@ -48,7 +50,53 @@ export const ImageLink = ({ linkName, bgImgUrl }) => {
                 >
                     <h2 style={{marginBottom: 0,}}>{linkName}</h2>
                 </Box>
-            </Card>
+            </Card> */}
+            <Paper
+                sx={{
+                    // width: '290px',
+                    height: '202px',
+                    position: 'relative',
+                    borderRadius: '3px', 
+                    '&::before': {
+                        content: '""',
+                        position: 'absolute',
+                        top: 0,
+                        right: 0,
+                        bottom: 0,
+                        left: 0,
+                        backgroundColor: 'rgba(5, 0, 10, 0.25)',
+                    }
+                }}
+                elevation={3}
+            >
+                <picture style={{borderRadius: '3px', }}>
+                    <source
+                        srcset={`${bgImgUrl}?width=290&height=202&format=jpg`}
+                        type="image/jpg"
+                    />
+                    <Image
+                        src={`${bgImgUrl}?width=290&height=202&format=auto`}
+                        alt={`${linkName} link image`}
+                        style={{
+                            width: '100%',
+                            height: '100%',
+                            position: 'absolute',
+                            objectFit: 'cover',
+                            borderRadius: '3px', 
+                        }}
+                    />
+                </picture>
+                <h2 style={{
+                        marginBottom: 0,
+                        position: 'absolute',
+                        bottom: 0,
+                        padding: '0 20px 12px',
+                        color: '#fff',
+                    }}
+                >
+                    {linkName}
+                </h2>
+            </Paper>
         </Link>
     )
 }
