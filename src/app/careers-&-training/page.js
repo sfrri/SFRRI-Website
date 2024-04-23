216aca7f91fcc1db224b65477efd981623ef27a4
Daniel Fraser <dan@danielfraser.me>
2024-04-23 14:22:44 +0100

    updated to use decap cms data

--- src/app/careers-&-training/page.js
+++ src/app/careers-&-training/page.js
@@ -1,5 +1,9 @@
 import * as React from 'react';
 import Paper from '@mui/material/Paper';
+import data from '../../../_data/careers-&-training.json'
+import Markdown from 'react-markdown'
+import rehypeExternalLinks from 'rehype-external-links'
+import rehypeRaw from 'rehype-raw'
 
 export default async function Obituaries() {
     //"use server";
@@ -7,8 +11,8 @@ export default async function Obituaries() {
         <main>
             <Paper variant="padded" elevation={3}>
                 <h2>Careers and Training</h2>
-
-                <h4>SFRR-Europe</h4>
+                <Markdown rehypePlugins={[[rehypeExternalLinks, { target: '_blank' }], [rehypeRaw]]}>{data.body}</Markdown>
+                {/* <h4>SFRR-Europe</h4>
                 <p>Travel Awards to attend SFRR-Europe Annual Meeting, Berlin, Germany 21-23 June 2017<br />
                     <a href="https://www.sfrr-europe.org/index.php/awards/travel-awards" target="_blank">sfrr-europe.org/index.php/awards/travel-awards</a></p>
 
@@ -19,7 +23,7 @@ export default async function Obituaries() {
 
                 <h4>SfRBM</h4>
                 <p>Redox Biology Symposium at Experimental Biology 24 April 2018<br />
-                    <a href="http://sfrbm.org/about/news/sfrbm-to-host-redox-biology-symposia-at-eb-2018-meeting" target="_blank">sfrbm.org/about/news/sfrbm-to-host-redox-biology-symposia-at-eb-2018-meeting</a></p>
+                    <a href="http://sfrbm.org/about/news/sfrbm-to-host-redox-biology-symposia-at-eb-2018-meeting" target="_blank">sfrbm.org/about/news/sfrbm-to-host-redox-biology-symposia-at-eb-2018-meeting</a></p> */}
             </Paper>
         </main>
     );
