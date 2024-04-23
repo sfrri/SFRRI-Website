77355b44311f095d55a9c196553939810220fc52
Daniel Fraser <dan@danielfraser.me>
2024-04-23 13:43:04 +0100

    updated config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+// const withBundleAnalyzer = require('@next/bundle-analyzer')()
+const withBundleAnalyzer = () => import ('@next/bundle-analyzer')
+ 
+export default process.env.ANALYZE === 'true' ? withBundleAnalyzer(nextConfig) : nextConfig
+
+//   npx cross-env ANALYZE=true npm run build
