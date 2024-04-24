796e2e59610cd7825e4a439cc559a81d0e3e9381
SFRRI Admin <admin@sfrr.org>
2024-04-24 17:48:50 +0100

    image component

--- src/app/presidential-address/page.js
+++ src/app/presidential-address/page.js
@@ -1,19 +1,45 @@
+'use client';
 import * as React from 'react';
+import { useState } from 'react';
 import Paper from '@mui/material/Paper';
 import Link from '@mui/material/Link';
 import data from '../../../_data/society/presidential-address.json'
 import Markdown from 'react-markdown'
 import rehypeExternalLinks from 'rehype-external-links'
+import Image from 'next/image';
+import Skeleton from '@mui/material/Skeleton';
 
-export default async function PresidentialAddress() {
-    "use server";
+export default function PresidentialAddress() {
+    const [loading, setLoading] = useState(true);
+    const maxImgWidth = /*imgWidth < 133.33 ? imgWidth :*/ 133.33
+    const maxImgHeight = /*imgHeight < 133.33 ? imgHeight :*/ 133.33
     return (
         <main>
             <Paper variant="padded" elevation={3}>
-                <div className="avatar" style={{ float: 'right', margin: '11px 18px', }}> 
-                    <img src={data.upload_path} alt='Giovanni Mann' />     
-                </div>
-                <Markdown rehypePlugins={[[rehypeExternalLinks, {target: '_blank'}]]}>{data.body}</Markdown>
+                {/* <div className="avatar" style={{ float: 'right', margin: '11px 18px', }}> */}
+                    {loading && <Skeleton
+                        variant="rounded"
+                        width={maxImgWidth}
+                        height={maxImgHeight}
+                    />}
+                    <Image
+                        priority
+                        width={maxImgWidth}
+                        height={maxImgHeight}
+                        onLoad={() => setLoading(false)}
+                        src={data.upload_path[0]}
+                        alt={data.name}
+                        style={{
+                            width: '100%',
+                            maxWidth: maxImgWidth,
+                            height: 'auto',
+                            display: loading ? 'none' : 'block',
+                            float: 'right',
+                            margin: '11px 18px',
+                        }}
+                    />
+                {/* </div> */}
+                <Markdown rehypePlugins={[[rehypeExternalLinks, { target: '_blank' }]]}>{data.body}</Markdown>
                 {/* <div className="avatar" style={{ float: 'right' }}> 
                     <img src={imgPresident} alt='Giovanni Mann' />     
                 </div> */}
