e2047746bafd15b87d379731b9395f4a9ae27522
Daniel Fraser <dan@danielfraser.me>
2024-04-25 16:58:55 +0100

    home bg img in next gen format

--- src/app/components/HomeNews.js
+++ src/app/components/HomeNews.js
@@ -1,17 +1,19 @@
 'use client';
 import * as React from 'react';
 import Paper from '@mui/material/Paper';
+import Image from 'next/image';
 
-export default function HomeNews({children}) {
+export default function HomeNews({ children }) {
+    const bgImgUrl = '/static/images/Hinge_top_vials_on_a_vial_rack.jpg'
     return (
-        <Paper 
-            variant="padded" 
+        <Paper
+            // variant="padded"
             className={'MuiPaper-elevation3'}
             sx={{
                 marginBottom: '10px',
-                backgroundImage: 'url(/static/images/Hinge_top_vials_on_a_vial_rack.jpg)',
-                backgroundRepeat: 'no-repeat',
-                backgroundSize: 'cover',
+                // backgroundImage: 'url(/static/images/Hinge_top_vials_on_a_vial_rack.jpg)',
+                // backgroundRepeat: 'no-repeat',
+                // backgroundSize: 'cover',
                 color: '#fff',
                 position: 'relative',
                 borderRadius: '3px',
@@ -25,6 +27,7 @@ export default function HomeNews({children}) {
                     left: 0,
                     backgroundColor: 'rgba(57, 51, 39, 0.65)',
                     backgroundColor: 'rgba(5, 0, 10, 0.75)',
+                    zIndex: 1,
                 },
                 '& a': {
                     color: '#fff',
@@ -34,7 +37,26 @@ export default function HomeNews({children}) {
                 },
             }}
         >
-            <>{children}</>
+            <picture style={{ borderRadius: '3px', }}>
+                <source
+                    srcSet={`${bgImgUrl}`}//?width=1280&height=720&format=jpg
+                    type="image/jpg"
+                />
+                <Image
+                    src={`${bgImgUrl}`}//?width=1280&height=720&format=auto
+                    alt='Hinge_top_vials_on_a_vial_rack'
+                    width={1280}
+                    height={720}
+                    style={{
+                        width: '100%',
+                        height: '100%',
+                        position: 'absolute',
+                        objectFit: 'cover',
+                        borderRadius: '3px',
+                    }}
+                />
+            </picture>
+            <span style={{zIndex:1,position:'relative',padding: '30px',display:'block',}}>{children}</span>
         </Paper>
     );
 }
