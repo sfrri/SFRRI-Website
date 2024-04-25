c3e8e59d3be2ee37644faad1027755377ea991f0
Daniel Fraser <dan@danielfraser.me>
2024-04-25 12:22:36 +0100

    optimisation experiment

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,42 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        optimizePackageImports: ['@emotion/cache'],
+        optimizePackageImports: ['@emotion/react'],
+        optimizePackageImports: ['@emotion/styled'],
+        optimizePackageImports: ['@maptiler/leaflet-maptilersdk'],
+        optimizePackageImports: ['@mui/icons-material'],
+        optimizePackageImports: ['@mui/material'],
+        optimizePackageImports: ['@next/bundle-analyzer'],
+        optimizePackageImports: ['"@react-google-maps/api'],
+        optimizePackageImports: ['isomorphic-dompurify'],
+        optimizePackageImports: ['leaflet'],
+        optimizePackageImports: ['next'],
+        optimizePackageImports: ['react'],
+        optimizePackageImports: ['react-dom'],
+        optimizePackageImports: ['react-markdown'],
+        optimizePackageImports: ['rehype-external-links'],
+        optimizePackageImports: ['rehype-raw'],
+      },
+};
 
 // const withBundleAnalyzer = require('@next/bundle-analyzer')()
-const withBundleAnalyzer = () => import ('@next/bundle-analyzer')
+// const withBundleAnalyzer = () => import ('@next/bundle-analyzer')
  
-export default process.env.ANALYZE === 'true' ? withBundleAnalyzer(nextConfig) : nextConfig
+// export default process.env.ANALYZE === 'true' ? withBundleAnalyzer(nextConfig) : nextConfig
 
 //   npx cross-env ANALYZE=true npm run build
+// module.exports = {
+//     experimental: {
+//       optimizePackageImports: ['@emotion/cache'],
+//       optimizePackageImports: ['@emotion/react'],
+//       optimizePackageImports: ['@emotion/styled'],
+//       optimizePackageImports: ['@maptiler/leaflet-maptilersdk'],
+//       optimizePackageImports: ['@mui/icons-material'],
+//       optimizePackageImports: ['@mui/material'],
+//       optimizePackageImports: ['@next/bundle-analyzer'],
+//       optimizePackageImports: ['"@react-google-maps/api'],
+//     },
+//   }
+
+export default nextConfig;
