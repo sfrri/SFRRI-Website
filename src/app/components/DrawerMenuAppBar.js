045bf6ad3ac038c520c7d840bff93c477958ef02
Daniel Fraser <dan@danielfraser.me>
2024-04-26 15:59:03 +0100

    updated sitemap page

--- src/app/components/DrawerMenuAppBar.js
+++ src/app/components/DrawerMenuAppBar.js
@@ -23,46 +23,9 @@ import ExpandMore from '@mui/icons-material/ExpandMore';
 import Container from '@mui/material/Container';
 import Image from 'next/image';
 import { CustomBreakpoints } from '@/utils';
+import navItems from './NavItems';
 
 const drawerWidth = 257;
-const navItems = [
-    { name: 'Home' },
-    {
-        name: 'Society',
-        subMenu: [
-            { name: 'Presidential Address' },
-            { name: 'SFRRI Executive' },
-            { name: 'Past Presidents' },
-            { name: 'History' },
-            { name: 'Governance & Byelaws' },
-            { name: 'SFRRI Awards & Fellowships' },
-            { name: 'Women in Science Forum' },
-            { name: 'Obituaries' },
-        ]
-    },
-    { name: 'SFRR Societies' },
-    { name: 'Membership' },
-    {
-        name: 'Conferences',
-        subMenu: [
-            { name: 'Future SFRRI Conferences' },
-            { name: 'Past SFRRI Conferences' },
-            { name: 'Past SFRRI Related Conferences' },
-            { name: 'Rules for Conference Organisation' },
-        ]
-    },
-    { name: 'Related Meetings' },
-    { name: 'SFRRI Outreach' },
-    { name: 'Journals' },
-    {
-        name: 'Education',
-        subMenu: [
-            { name: 'Education' },
-            { name: 'Careers & Training' },
-        ]
-    },
-];
-
 
 function DrawerMenuAppBar(props) {
     const { window } = props;
--- src/app/sitemap/page.js
+++ src/app/sitemap/page.js
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import Paper from '@mui/material/Paper';
 import Link from '@mui/material/Link';
+import navItems from '../components/NavItems';
+import { kebabize } from '@/utils';
 
 export default async function Sitemap() {
     "use server";
@@ -9,38 +11,18 @@ export default async function Sitemap() {
             <Paper variant="padded" elevation={3}>
                 <h2>Sitemap</h2>
                 <ul>
-                    <li><Link href="/">Home</Link></li>
-                    <li>Society
-                        <ul>
-                            <li><Link href="presidential-address">Presidential Address</Link></li>
-                            <li><Link href="sfrri-executive">SFRRI Executive</Link></li>
-                            <li><Link href="past-presidents">Past Presidents</Link></li>
-                            <li><Link href="history">History</Link></li>
-                            <li><Link href="governance">Governance & Byelaws</Link></li>
-                            <li><Link href="sfrri-awards-&-fellowships">SFRRI Awards & Fellowships</Link></li>
-                            <li><Link href="women-in-science-forum">Women in Science Forum</Link></li>
-                            <li><Link href="obituaries">Obituaries</Link></li>
-                        </ul>
-                    </li>
-                    <li><Link href="sfrri-societies">SFRR Societies</Link></li>
-                    <li><Link href="membership">Membership</Link></li>
-                    <li>Conferences
-                        <ul>
-                            <li><Link href="future-sfrri-conferences">Future SFRRI Conferences</Link></li>
-                            <li><Link href="past-sfrri-conferences">Past SFRRI Conferences</Link></li>
-                            <li><Link href="past-sfrri-related-conferences">Past SFRRI Related Conferences</Link></li>
-                            <li><Link href="rules-for-conference-organisation">Rules for Conference Organisation</Link></li>
-                        </ul>
-                    </li>
-                    <li><Link href="related-meetings">Related Meetings</Link></li>
-                    <li><Link href="sfrri-outreach">SFRRI Outreach</Link></li>
-                    <li><Link href="journals">Journals</Link></li>
-                    <li>Education
-                        <ul>
-                            <li><Link href="education">Education</Link></li>
-                            <li><Link href="careers-&-training">Careers & Training</Link></li>
-                        </ul>
-                    </li>
+                    {navItems.map(item => (
+                        item.subMenu != undefined ?
+                            <li>{item.name}
+                                <ul>
+                                    {item.subMenu.map(subItem => {
+                                        return <li><Link href={kebabize(subItem.name)} key={subItem.name}>{subItem.name}</Link></li>
+                                    })}
+                                </ul>
+                            </li>
+                            :
+                            <li><Link href={item.name === 'Home' ? '' : kebabize(item.name)} key={item.name}>{item.name}</Link></li>
+                    ))}
                 </ul>
                 <ul>
                     <li><Link href="sitemap">Sitemap</Link></li>
