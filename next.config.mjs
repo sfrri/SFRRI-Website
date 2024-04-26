4436bb393cde554194b32edddf07595da504d4dd
Daniel Fraser <dan@danielfraser.me>
2024-04-26 12:25:51 +0100

    added avif output

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        formats: ['image/webp'],
+        formats: ['image/avif', 'image/webp'],
       },
     experimental: {
         optimizePackageImports: ['@emotion/cache'],
