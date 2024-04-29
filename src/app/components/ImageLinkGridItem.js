5aba81ceceba81816cd2f17ebf63959cedf9e818
SFRRI Admin <admin@sfrr.org>
2024-04-29 21:18:48 +0100

    created ImageLoading component

--- src/app/components/ImageLinkGridItem.js
+++ src/app/components/ImageLinkGridItem.js
@@ -1,21 +1,13 @@
-'use client';
 import * as React from 'react';
-import { useState } from 'react';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Link from '@mui/material/Link';
-import Image from 'next/image';
-// import Skeleton from '@mui/material/Skeleton';
-// import DOMPurify from 'isomorphic-dompurify';
 import Markdown from 'react-markdown'
 import rehypeExternalLinks from 'rehype-external-links'
 import rehypeRaw from 'rehype-raw'
-import CircularProgress from '@mui/material/CircularProgress';
+import ImageLoading from './ImageLoading';
 
-const ImageLinkGridItem = ({ xs, md, href,/* width, height,*/ src, name, body }) => {
-    const [loading, setLoading] = useState(true);
-    const maxImgWidth = /*imgWidth < 133.33 ? imgWidth :*/ 133.33
-    const maxImgHeight = /*imgHeight < 133.33 ? imgHeight :*/ 133.33
+const ImageLinkGridItem = ({ xs, md, href, src, name, body }) => {
 
     return (
         href && src && name ? <Grid item xs={xs} md={md}>
@@ -30,48 +22,7 @@ const ImageLinkGridItem = ({ xs, md, href,/* width, height,*/ src, name, body })
                     textAlign: 'center',
                 }}
             >
-                {/* {loading && <Skeleton 
-                                variant="rounded" 
-                                width={maxImgWidth} 
-                                height={maxImgHeight} 
-                            />
-                } */}
-                <div style={{
-                    width: maxImgWidth,
-                    height: maxImgHeight,
-                    position: 'relative'
-                }}>
-                    {loading &&
-
-                        <CircularProgress
-                            sx={{
-                                position: 'absolute',
-                                top: '30%',
-                                left: '50%',
-                                translate: '-50% -50%',
-                            }}
-                        />
-                    }
-                    <Image
-                        priority
-                        width={maxImgWidth}
-                        height={maxImgHeight}
-                        onLoad={(e) => setLoading(false)}
-                        src={src}
-                        alt={`${name}`}
-                        style={{
-                            width: 'auto',
-                            maxWidth: maxImgWidth,
-                            height: 'auto',
-                            maxHeight: maxImgHeight,
-                            // display: loading ? 'none' : 'block',
-                            opacity: loading ? 0 : 1,
-                            transitionProperty: 'opacity',
-                            transitionDuration: '500ms',
-                            transitionTimingFunction: 'cubic-bezier(0.7, 0, 0.6, 1)',
-                        }}
-                    />
-                </div>
+                {name && <ImageLoading imgSrc={src} name={name} />}
                 <p>{name}</p>
             </Box>
             {body &&
---
+++ src/app/components/ImageLoading.js
@@ -0,0 +1,53 @@
+'use client';
+import * as React from 'react';
+import { useState } from 'react';
+import Image from 'next/image';
+import CircularProgress from '@mui/material/CircularProgress';
+
+const ImageLoading = ({ imgSrc, name, prAdd }) => {
+    const [loading, setLoading] = useState(true);
+    const maxImgWidth = /*imgWidth < 133.33 ? imgWidth :*/ 133.33
+    const maxImgHeight = /*imgHeight < 133.33 ? imgHeight :*/ 133.33
+
+    return (
+        <div style={{
+            width: maxImgWidth,
+            height: maxImgHeight,
+            position: 'relative',
+            float: prAdd ? 'right' : 'auto', 
+            margin: prAdd ? '11px 18px' : 0,
+        }}>
+            {loading &&
+
+                <CircularProgress
+                    sx={{
+                        position: 'absolute',
+                        top: '30%',
+                        left: '50%',
+                        translate: '-50% -50%',
+                    }}
+                />
+            }
+            <Image
+                priority
+                width={maxImgWidth}
+                height={maxImgHeight}
+                onLoad={() => setLoading(false)}
+                src={imgSrc}
+                alt={name}
+                style={{
+                    width: '100%',
+                    maxWidth: maxImgWidth,
+                    height: 'auto',
+                    // display: loading ? 'none' : 'block',
+                    opacity: loading ? 0 : 1,
+                    transitionProperty: 'opacity',
+                    transitionDuration: '500ms',
+                    transitionTimingFunction: 'cubic-bezier(0.7, 0, 0.6, 1)',
+                }}
+            />
+        </div>
+    )
+}
+
+export default ImageLoading
