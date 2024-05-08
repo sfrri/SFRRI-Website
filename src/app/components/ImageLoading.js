8e57442823d14b87dde3f6e79b8ec1639563fa88
SFRRI Admin <admin@sfrr.org>
2024-05-08 17:07:22 +0100

    president's address avatar position for mobile screens

--- src/app/components/ImageLoading.js
+++ src/app/components/ImageLoading.js
@@ -3,6 +3,8 @@ import * as React from 'react';
 import { useState } from 'react';
 import Image from 'next/image';
 import CircularProgress from '@mui/material/CircularProgress';
+import { CustomBreakpoints } from '@/utils';
+import Box from '@mui/material/Box';
 
 const ImageLoading = ({ imgSrc, name, prAdd }) => {
     const [loading, setLoading] = useState(true);
@@ -10,43 +12,46 @@ const ImageLoading = ({ imgSrc, name, prAdd }) => {
     const maxImgHeight = /*imgHeight < 133.33 ? imgHeight :*/ 133.33
 
     return (
-        <div style={{
-            width: maxImgWidth,
-            height: '180px',
-            position: 'relative',
-            float: prAdd ? 'right' : 'auto', 
-            margin: prAdd ? '11px 18px' : 0,
-        }}>
-            {loading &&
+        <CustomBreakpoints>
+            <Box sx={{
+                width: maxImgWidth,
+                height: prAdd ? 'auto' : '180px',
+                position: 'relative',
+                float: { xs: 'left', sm: 'right' } ,
+                margin: prAdd ? '11px 18px' : 0,
+            }}>
+                {console.log( prAdd ? { xs: 'left', sm: 'right' } : 'auto',)}
+                {loading &&
 
-                <CircularProgress
-                    sx={{
-                        position: 'absolute',
-                        top: '30%',
-                        left: '50%',
-                        translate: '-50% -50%',
+                    <CircularProgress
+                        sx={{
+                            position: 'absolute',
+                            top: '30%',
+                            left: '50%',
+                            translate: '-50% -50%',
+                        }}
+                    />
+                }
+                <Image
+                    priority
+                    width={maxImgWidth}
+                    height={maxImgHeight}
+                    onLoad={() => setLoading(false)}
+                    src={imgSrc}
+                    alt={name}
+                    style={{
+                        width: '100%',
+                        maxWidth: maxImgWidth,
+                        height: 'auto',
+                        // display: loading ? 'none' : 'block',
+                        opacity: loading ? 0 : 1,
+                        transitionProperty: 'opacity',
+                        transitionDuration: '500ms',
+                        transitionTimingFunction: 'cubic-bezier(0.7, 0, 0.6, 1)',
                     }}
                 />
-            }
-            <Image
-                priority
-                width={maxImgWidth}
-                height={maxImgHeight}
-                onLoad={() => setLoading(false)}
-                src={imgSrc}
-                alt={name}
-                style={{
-                    width: '100%',
-                    maxWidth: maxImgWidth,
-                    height: 'auto',
-                    // display: loading ? 'none' : 'block',
-                    opacity: loading ? 0 : 1,
-                    transitionProperty: 'opacity',
-                    transitionDuration: '500ms',
-                    transitionTimingFunction: 'cubic-bezier(0.7, 0, 0.6, 1)',
-                }}
-            />
-        </div>
+            </Box>
+        </CustomBreakpoints>
     )
 }
 
