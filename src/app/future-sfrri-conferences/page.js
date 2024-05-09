c2a05d071999e6db7cb667f171c2a296ccdcf787
SFRRI Admin <admin@sfrr.org>
2024-05-09 17:12:21 +0100

    added divivider hr between meeting items

--- src/app/future-sfrri-conferences/page.js
+++ src/app/future-sfrri-conferences/page.js
@@ -3,6 +3,7 @@ import Paper from '@mui/material/Paper';
 import data from '../../../_data/conferences/future-sfrri-conferences.json'
 import NewsItem from '../components/NewsComponents'
 import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
 
 export default function FutureSFRRIConferences() {
     return (
@@ -10,15 +11,18 @@ export default function FutureSFRRIConferences() {
             <Paper variant="padded" elevation={3}>
                 <Typography variant="h2">Future SFRRI Conferences</Typography>
                 {data.future_sfrri_conferences && data.future_sfrri_conferences.map(item => (
-                    <NewsItem
-                        date={item.date}
-                        title={item.meeting_title}
-                        location={item.location}
-                        link={item.further_information_link_hypertext ? item.further_information_link_hypertext : item.upload_path[0]}
-                        linkText={item.further_information_link_text}
-                        body={item.body}
-                        key={item.meeting_title}
-                    />
+                    <React.Fragment>
+                        <NewsItem
+                            date={item.date}
+                            title={item.meeting_title}
+                            location={item.location}
+                            link={item.further_information_link_hypertext ? item.further_information_link_hypertext : item.upload_path[0]}
+                            linkText={item.further_information_link_text}
+                            body={item.body}
+                            key={item.meeting_title}
+                        />
+                        <Divider orientation="horizontal" flexItem sx={{ marginTop: '0 !important', }} />
+                    </React.Fragment>
                 ))}
             </Paper>
         </main>
--- src/app/past-sfrri-related-conferences/page.js
+++ src/app/past-sfrri-related-conferences/page.js
@@ -3,6 +3,7 @@ import Paper from '@mui/material/Paper';
 import data from '../../../_data/conferences/past-sfrri-related-conferences.json'
 import NewsItem from '../components/NewsComponents'
 import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
 
 export default function SFRRIRelatedConferences() {
     return (
@@ -10,15 +11,18 @@ export default function SFRRIRelatedConferences() {
             <Paper variant="padded" elevation={3}>
                 <Typography variant="h2">Past SFRRI Related Conferences</Typography>
                 {data.past_sfrri_related_conferences && data.past_sfrri_related_conferences.map(item => (
-                    <NewsItem
-                        date={item.date}
-                        title={item.meeting_title}
-                        location={item.location}
-                        link={item.further_information_link_hypertext ? item.further_information_link_hypertext : item.upload_path[0]}
-                        linkText={item.further_information_link_text}
-                        body={item.body}
-                        key={item.meeting_title}
-                    />
+                    <React.Fragment>
+                        <NewsItem
+                            date={item.date}
+                            title={item.meeting_title}
+                            location={item.location}
+                            link={item.further_information_link_hypertext ? item.further_information_link_hypertext : item.upload_path[0]}
+                            linkText={item.further_information_link_text}
+                            body={item.body}
+                            key={item.meeting_title}
+                        />
+                        <Divider orientation="horizontal" flexItem sx={{ marginTop: '0 !important', }} />
+                    </React.Fragment>
                 ))}
             </Paper>
         </main>
