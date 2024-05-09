2dda8b30668434833826639352158eb4af467acc
SFRRI Admin <admin@sfrr.org>
2024-05-09 16:30:26 +0100

    simplified input for SFRRI Outreach

--- src/app/sfrri-outreach/page.js
+++ src/app/sfrri-outreach/page.js
@@ -1,18 +1,28 @@
 import * as React from 'react';
 import Paper from '@mui/material/Paper';
-import data from '../../../_data/sfrri-outreach.json'
-import Markdown from 'react-markdown'
-import rehypeExternalLinks from 'rehype-external-links'
-import rehypeRaw from 'rehype-raw'
+import data from '../../../_data/sfrri-outreachnew.json'
 import Typography from '@mui/material/Typography';
 
 export default async function SFRRISponsoredConferences() {
-    "use server";
     return (
         <main>
             <Paper variant="padded" elevation={3}>
                 <Typography variant="h2">SFRRI Sponsored Conferences</Typography>
-                {data.body && <Markdown rehypePlugins={[[rehypeExternalLinks, {target: '_blank'}], [rehypeRaw]]}>{data.body}</Markdown>}
+                {data.outreach_years && data.outreach_years.map(item => {
+                    return <React.Fragment key={item.year}><h4>{item.year}</h4>
+                        {item.meetings.map(entry => {
+                            return <p>
+                                <a
+                                    href={entry.link_hypertext ? entry.link_hypertext : entry.upload_path[0]}
+                                    target="_blank"
+                                    key={entry.link_text}
+                                >
+                                    {entry.link_text}
+                                </a>
+                            </p>
+                        })}
+                    </React.Fragment>
+                })}
             </Paper>
         </main>
     );
