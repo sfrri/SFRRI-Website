7d5b894fb0c18b36aa6fe759bd0f769e129bbdc5
Daniel Fraser <dan@danielfraser.me>
2024-05-15 13:18:20 +0100

    experiment with Suspense wrapping page content

--- src/app/components/NewsConditional.js
+++ src/app/components/NewsConditional.js
@@ -5,27 +5,31 @@ import Grid from "@mui/material/Grid";
 import { News } from "./News";
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
+import { Suspense } from "react";
+import Skeleton from "@mui/material/Skeleton";
 
-const NewsConditional = ({children}) => {
+const NewsConditional = ({ children }) => {
     const pathname = usePathname();
-    
+
     return (
         <Container maxWidth="lg" sx={{ position: 'relative', marginTop: '10px', }}>
-            {pathname === '/' ?
-                <>{children}</>
-                :
-                <Grid container spacing={2}>
-                    <Grid item xs={12} md={8.5} lg={9}>
-                        {children}
+            <Suspense fallback={<Skeleton />}>
+                {pathname === '/' ?
+                    <>{children}</>
+                    :
+                    <Grid container spacing={2}>
+                        <Grid item xs={12} md={8.5} lg={9}>
+                            {children}
+                        </Grid>
+                        <Grid item xs={12} md={3.5} lg={3}>
+                            <Paper variant="padded" elevation={3}>
+                                <Typography variant="h2">News</Typography>
+                                <News />
+                            </Paper>
+                        </Grid>
                     </Grid>
-                    <Grid item xs={12} md={3.5} lg={3}>
-                        <Paper variant="padded" elevation={3}>
-                        <Typography variant="h2">News</Typography>
-                            <News />
-                        </Paper>
-                    </Grid>
-                </Grid>
-            }
+                }
+            </Suspense>
         </Container>
     )
 }
