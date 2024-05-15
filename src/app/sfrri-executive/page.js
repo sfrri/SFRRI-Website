9ff89abc4d507d2640c12b7acefebbca1f57f1c3
SFRRI Admin <admin@sfrr.org>
2024-05-15 17:22:16 +0100

    updated data key

--- src/app/sfrri-executive/page.js
+++ src/app/sfrri-executive/page.js
@@ -9,7 +9,7 @@ export default function SFRRIExecutive() {
         <main>
             <Paper variant="padded" elevation={3}>
                 <Typography variant="h2" sx={{ marginBottom: '1em', }}>SFRRI Executive</Typography>
-                {data.sfrri_executive_member && data.sfrri_executive_member.map(item => (
+                {data.sfrri_executive_members && data.sfrri_executive_members.map(item => (
                     <SFRRIExecutiveItem
                         imgSrc={item.upload_path}
                         title={item.title}
