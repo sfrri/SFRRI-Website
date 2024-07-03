b7e649ed9e62e92e2695adec478936e0d572b6a1
SFRRI Admin <admin@sfrr.org>
2024-07-03 19:00:25 +0100

    min height added to avoid jump on load

--- src/app/components/ImageLoading.js
+++ src/app/components/ImageLoading.js
@@ -17,6 +17,7 @@ const ImageLoading = ({ imgSrc, name, prAdd }) => {
                 width: maxImgWidth,
                 // height: prAdd ? 'auto' : '180px',
                 height: 'auto',
+                minHeight: '140px',
                 position: 'relative',
                 float: prAdd ? { xs: 'left', sm: 'right' } : 'auto',
                 margin: prAdd ? { xs: '0 100px 11px 0', sm: '11px 18px' } : 0,
