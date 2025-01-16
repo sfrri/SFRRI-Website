da95b75be440a6224759cee4fb5e571650a8184d
SFRRI Admin <admin@sfrr.org>
2025-01-16 13:19:13 +0000

    read/close bio on accordion closed/open

--- src/app/components/BioAccordion.js
+++ src/app/components/BioAccordion.js
@@ -1,3 +1,4 @@
+'use client';
 import * as React from 'react';
 import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
@@ -6,9 +7,15 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { MarkdownComponent } from '@/utils';
 
 export default function AccordionUsage({bio}) {
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
+
   return (
     <div>
-      <Accordion elevation={0}>
+      <Accordion elevation={0} onChange={handleChange('panel1')}>
         <AccordionSummary
           expandIcon={<ExpandMoreIcon />}
           aria-controls="panel-content"
@@ -19,7 +26,7 @@ export default function AccordionUsage({bio}) {
             fontWeight: 500,
           }}
         >
-          Read Bio
+          {!expanded ? 'Read Bio' : 'Close Bio'}
         </AccordionSummary>
         <AccordionDetails>
             {bio && <MarkdownComponent>{bio}</MarkdownComponent>}
