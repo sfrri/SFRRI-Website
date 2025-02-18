06307d7843594f6b6c6dbd14fd7f1269601d33ea
SFRRI Admin <admin@sfrr.org>
2025-02-18 17:45:17 +0000

    added body data to SFRRI Outreach page

--- src/app/sfrri-outreach/page.js
+++ src/app/sfrri-outreach/page.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import data from '../../../_data/sfrri-outreach.json'
 import Typography from '@mui/material/Typography';
+import { MarkdownComponent } from '@/utils';
 import SFRRIOutreachItem from '../components/SFRRIOutreachItem';
 
 const title = 'SFRRI Sponsored Conferences'
@@ -13,6 +14,7 @@ export default async function SFRRISponsoredConferences() {
     return (
         <React.Fragment>
             <Typography variant="h2">{title}</Typography>
+            {data.body && <MarkdownComponent>{data.body}</MarkdownComponent>}
             {data.outreach_years && data.outreach_years.map(item => {
                 return <React.Fragment key={item.year}>
                     <Typography variant='h4'>{item.year}</Typography>
